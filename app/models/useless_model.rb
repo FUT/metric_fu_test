d7c3a139f3799e1bbb9a84818e72e7b7557d43f8
Jura <fut.wrk@gmail.com>
2012-01-11 21:24:54 +0300

    test model was changed 4

--- app/models/useless_model.rb
+++ app/models/useless_model.rb
@@ -4,10 +4,23 @@ class UselessModel < ActiveRecord::Base
 
   @array = %w{Do not write such useless code !}
 
-  def some_m
+  def some_method
     @array.each do |element|
       p element
       not_used = element + element * element / 5 - 17 ** 12 + 12 ** 17
     end
+  end
+
+  def same_method
+    one = 1
+
+    @array.each do |element|
+      p element + 3
+      not_used = element + 2 * element / 7 - 4 ** 5 + 5 ** 6
+    end
+
+    p one
+    p one+one
+    p one+one+one
   end
 end

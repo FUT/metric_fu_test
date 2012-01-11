75f251b86ea57273200acc01d85f39b68281ec62
Jura <fut.wrk@gmail.com>
2012-01-11 21:40:45 +0300

    test model was changed 6

--- app/models/useless_model.rb
+++ app/models/useless_model.rb
@@ -44,5 +44,67 @@ class UselessModel < ActiveRecord::Base
     end
   end
 
+  def ten_conditions
+    one = 1
+    thee = 3
+    if one + three + 1 == 5
+      p 'five'
+    else
+      p 'not five'
+      four = 4
+      
+      if four = 5
+        four = 4
+      end
+      
+      if four > 0
+        p -1
+        if four > 1
+          p 0
+          if four > 2
+            p 1
+          else
+            p 2
+          end
+          p 3
+        else
+          p 4
+        end
+        p 5
+      else
+        p 6
+      end
+      p 7
+    end
+    p 8
+  end
+  
+  def try_to_understand_me
+    one = 1
+    zero = ""
+    fail = eval "#{one + zero.send(eval "#{one > zero.length ? 'length' : 'divide_by_zero'}.to_sym") < 1 ? 'fail' : 'fail'}"
+    if one == eval("1 if fail == eval('fail')")
+      10.times do
+        eval "p one"
+        9.times do
+          eval "p one"
+          8.times do
+            eval "p one"
+            7.times do
+              eval "p one"
+              6.times do
+                eval "p one"
+              end
+              eval "p one"
+            end
+            eval "p one"
+          end
+          eval "p one"
+        end
+        eval "p one"
+      end
+      eval "p one"
+    end
 
+  end
 end
